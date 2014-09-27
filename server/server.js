90499dbda8c06536c9053f8df808dd66af0688dd
Jim Pick <jim@backblend.com>
2014-09-27 14:41:30 -0700

    Compile template at startup time.
    No need to fetch it from disk on each request.

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ var path = require('path')
   , fs = require('fs')
   , http = require('http')
   , koa = require('koa')
+  , co = require('co')
   , thunkify = require('thunkify')
   , staticCache = require('koa-static-cache')
   , _ = require('underscore')
@@ -10,29 +11,40 @@ var path = require('path')
   , wwwDir = path.join(__dirname, '../www')
   , staticDir = path.join(wwwDir, 'static')
 
-app.use(staticCache(staticDir, {
-  prefix: '/static/'
-, maxAge: 365 * 24 * 60 * 60
-}))
+function setupStatic() {
+  app.use(staticCache(staticDir, {
+    prefix: '/static/'
+  , maxAge: 365 * 24 * 60 * 60
+  }))
+}
 
-
-app.use(function *welcome(next) {
-  // FIXME if (this.request.path != '/') return yield next
-
-  var template = yield read(path.join(wwwDir, 'index.html'), 'utf8')
-  this.type = 'html'
-  this.body = _.template(template)({
+function setupWelcome(template) {
+  var compiledTemplate = _.template(template)({
     config: {
       canonicalUrl: 'localhost'
     }
   , staticBase: '/static'
   })
-})
+  app.use(function *welcome(next) {
+    // FIXME if (this.request.path != '/') return yield next
+    this.type = 'html'
+    this.body = compiledTemplate
+  })
+}
 
-var server = http.createServer(app.callback())
-  , port = process.env.PORT || 4126
+co(function *initialize() {
+  // Things to do before handling requests
+  var template = yield read(path.join(wwwDir, 'index.html'), 'utf8')
 
-server.listen(port)
+  // Setup request handling
+  setupStatic()
+  setupWelcome(template)
 
-console.log('Listening on port ' + port)
+  // Start listening
+  var server = http.createServer(app.callback())
+    , port = process.env.PORT || 4126
+  server.listen(port)
+  console.log('Listening on port ' + port)
+})()
+
 

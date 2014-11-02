078e578f2202f1be416799326506c605693adb71
Jim Pick <jim@backblend.com>
2014-11-02 14:11:24 -0800

    Add replication to local couchdb.
    We want this to replicate via a proxy to a per-user db on Cloudant.

--- www/static/models/setupPouch.js
+++ www/static/models/setupPouch.js
@@ -12,16 +12,25 @@ define([
     if (!currentUser.isLoggedIn())
       return
 
-    var dbname = 'todos-' + currentUser.id
+    var dbName = 'todos-' + currentUser.id
 
     // Save all of the todo items in the `"todos-backbone"` database.
     Backbone.sync = BackbonePouch.sync({
       // We currently suffix by the PouchDB version here
       // because at the moment PouchDB does not support upgrade
-      db: new PouchDB(dbname)
+      db: new PouchDB(dbName)
     , listen: true
     , fetch: 'query'
     })
+
+    // Replication
+    var remoteUrl = 'http://localhost:5984/todos'
+    PouchDB.replicate(dbName, remoteUrl, {continuous: true})
+    PouchDB.replicate(remoteUrl, dbName, {continuous: true})
+
+    // FIXME: This will keep replicating even after the user logs out
+    // It would be nice if the replication stopped when the user logs
+    // out to conserve resources.
   }
 
   return {

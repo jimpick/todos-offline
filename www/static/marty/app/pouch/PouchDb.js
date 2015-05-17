3369c16f609d6a47d3b4b1d1cfab04e1f4ec23a0
Jim Pick <jim@backblend.com>
2015-05-17 12:21:45 -0700

    Sync with Cloudant
    This is pretty crude. It's not hooked into the UI yet.

--- www/static/marty/app/pouch/PouchDb.js
+++ www/static/marty/app/pouch/PouchDb.js
@@ -9,6 +9,27 @@ if (! window.globalOpts ||
 var dbName = 'todos-' + window.globalOpts.user._id
 var db = new PouchDB(dbName)
 
+// Replication
+var remoteUrl = 'https://' +
+  window.globalOpts.user.apiKey + ':' +
+  window.globalOpts.user.apiPassword + '@' +
+  window.globalOpts.cloudant.account + '.cloudant.com/' +
+  encodeURIComponent(window.globalOpts.cloudant.db + '/users/' +
+                     window.globalOpts.user._id)
+
+db.replicate.to(remoteUrl, {continuous: true})
+.on('error', function(err) {
+  console.log('Replication error (to remote)', err)
+})
+db.replicate.from(remoteUrl, {continuous: true})
+.on('error', function(err) {
+  console.log('Replication error (from remote)', err)
+})
+
+// FIXME: This will keep replicating even after the user logs out
+// It would be nice if the replication stopped when the user logs
+// out to conserve resources.
+
 module.exports = {
   db: db
 }
